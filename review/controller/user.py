64027c5bb54fa858a676a4a9f3ac7a549faae46d
R.J. Aquino <rjtaquino@gmail.com>
2014-08-03 14:19:36 -0400

    Added logout and a home page

--- review/controller/user.py
+++ review/controller/user.py
@@ -1,6 +1,6 @@
 from review import app, db
 import review.model.user as user_model
-from flask import render_template, request, session
+from flask import render_template, request, session, redirect, url_for
 import crypt
 
 # logs in the user with provided name and password (or shows login form)
@@ -9,23 +9,37 @@ def login():
     # perform the login
     if request.method == "POST":
         #if email or password not provided, fail
+        if request.form["email"] == '' or request.form["password"] == '':
+            return render_template("error.html", error="Please enter an email address and password.")
+
         # else, look for email in db
         user = user_model.get({"email": request.form["email"]}, one=True)
+
         # if no user found, fail
         if user is None:
-            # XXX need to escape?
+            # escaping is done in template
             return render_template("error.html", error="No account for %s found" % request.form["email"])
+
+        # check if password correct
+        elif user["password"] == crypt.crypt(request.form["password"], user["password"]):
+            session["user_id"] = user["id"]
+            return redirect(url_for('home'))
         else:
-            if user["password"] == crypt.crypt(request.form["password"], user["password"]):
-                session["user_id"] = user["id"]
-                return render_template("home.html", user=session["user_id"])
-            else:
-                return render_template("error.html", error="Invalid password.")
+            return render_template("error.html", error="Invalid password.")
     else:
         return render_template("login.html")
 
 
+# logs out the current user
 @app.route('/logout', methods=['GET'])
 def logout():
     session.clear()
+    return redirect(url_for('home'))
+
+# default for now. should be in different controller
+@app.route('/')
+def home():
+    # pull user from session
+    user = session.get("user_id", None)
+    return render_template("home.html", user=user)
 
