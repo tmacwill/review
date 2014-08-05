bc0322afa9f1cc4535ee81907a5ca5a1bf6f5a78
R.J. Aquino <rjtaquino@gmail.com>
2014-08-05 04:08:10 -0400

    rename migrations

--- schema/migrations/4-add-upload_files-table.sql
+++ schema/migrations/4-add-upload_files-table.sql
@@ -1,10 +1,10 @@
-CREATE TABLE submission_files (
+CREATE TABLE upload_files (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    submission_id BIGINT UNSIGNED NOT NULL,
+    upload_id BIGINT UNSIGNED NOT NULL,
     filename VARCHAR(255) NOT NULL,
     contents MEDIUMTEXT,
     creation_time BIGINT UNSIGNED NOT NULL,
     PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE submission_files ADD INDEX submission_id (submission_id);
+ALTER TABLE uploads ADD INDEX upload_id (upload_id);
