7b6a87b079db218a8873eaa94668a269f4b5f0b7
R.J. Aquino <rjtaquino@gmail.com>
2014-08-07 01:18:14 -0400

    Fixed python3 compat for linter, added cmdline arg

--- lint/lint.py
+++ lint/lint.py
@@ -1,5 +1,7 @@
+import argparse
 import os
 import subprocess
+import sys
 
 RED = "\033[31m"
 YELLOW = "\033[33m"
@@ -7,6 +9,13 @@ GREEN = "\033[32m"
 DEFAULT = "\033[39m"
 
 to_print = {"E": RED, "W": YELLOW, "C": YELLOW, "F": YELLOW}
+
+
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--include-warnings', dest="include_warnings", action='store_true', help='include warnings/cautions')
+args = parser.parse_args()
+
 path = os.path.dirname(os.path.realpath(__file__))
 rcfile = path + '/pylint-rc'
 
@@ -17,16 +26,20 @@ for root, dirs, files in os.walk(os.path.dirname(path)):
             py_files.append(os.path.join(root, file))
 py_files.sort()
 
-p = subprocess.Popen('pylint --reports=n --rcfile=%s %s' % (rcfile, " ".join(py_files)), shell=True, stdout=subprocess.PIPE)
+error_string = "" if args.include_warnings else " --errors-only "
+p = subprocess.Popen('pylint --reports=n --rcfile=%s %s %s' % (rcfile, error_string, " ".join(py_files)), shell=True, stdout=subprocess.PIPE)
 out, err = p.communicate()
+
 file_line = ""
 printed = False
 for line in out.splitlines():
+    # python3 sends bytestrings
+    line = line.decode('utf-8')
     if line[0] == '*':
         file_line = line
         printed = False
     elif line[0] in to_print:
         if not printed:
             printed = True
-            print file_line
-        print to_print[line[0]],line,DEFAULT
+            print(file_line)
+        print(to_print[line[0]]+line+DEFAULT)
