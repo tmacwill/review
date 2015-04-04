4ecfc4d63f8471eb716afe2bfa1ca938abe8b355
R.J. Aquino <rjtaquino@gmail.com>
2015-04-04 08:13:41 +0000

    Profile uses real data
    Also patched up some issues with the user model
    Eventually, upload model will need to be paginated, and some more work
    will need to be done to populate profile stats

--- r/__init__.py
+++ r/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask.ext.babel import Babel
 
 app = Flask(__name__)
+babel = Babel(app)
 
 # this is just a development key. production will use a key not checked into the repo.
 app.secret_key = b'R\xdaF[f\xed\xc9\xd5\x81P\xec\xdb4G\xb8\xc6\xecj%\x13\x00\x1a\x08c'
